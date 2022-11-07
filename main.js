1ef7f01316835772bf7827efadea252961966ee0
Ryan <116980194+RSnow20@users.noreply.github.com>
2022-11-06 22:32:48 -0500

    Added all necessary files for first project in Odin

--- main.js
+++ main.js
@@ -1,23 +1,74 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+let wins = 0;
+let loses = 0;
+let ties = 0;
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function getComputerChoice() {
+  let computerChoice = "";
+  let numberChoice = Math.floor(Math.random() * 3 + 1);
 
-setupCounter(document.querySelector('#counter'))
+  if (numberChoice === 1) {
+    computerChoice = "Rock";
+  } else if (numberChoice === 2) {
+    computerChoice = "Paper";
+  } else if (numberChoice === 3) {
+    computerChoice = "Scissors";
+  }
+
+  return computerChoice;
+}
+
+function playRound(playerChoice, computerChoice) {
+  playerChoice = playerChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+
+  if (playerChoice === "rock") {
+    if (computerChoice === "rock") {
+      ties++;
+      return "It's a tie! Both oppponents chose rock!";
+    } else if (computerChoice === "paper") {
+      loses++;
+      return "Computer wins! Paper covers rock!";
+    } else if (computerChoice === "scissors") {
+      wins++;
+      return "You win! Rock smashes scissors!";
+    }
+  } else if (playerChoice === "paper") {
+    if (computerChoice === "rock") {
+      wins++;
+      return "You win! Paper covers rock!";
+    } else if (computerChoice === "paper") {
+      ties++;
+      return "It's a tie! Both oppponents chose paper!";
+    } else if (computerChoice === "scissors") {
+      loses++;
+      return "You lose! Scissors cut paper!";
+    }
+  } else if (playerChoice === "scissors") {
+    if (computerChoice === "rock") {
+      loses++;
+      return "You lose! Rock smashes scissors!";
+    } else if (computerChoice === "paper") {
+      wins++;
+      return "You win! Scissors cut paper!";
+    } else if (computerChoice === "scissors") {
+      ties++;
+      return "It's a tie! Both oppponents chose scissors!";
+    }
+  } else {
+    return "Something went wrong! Please try again!";
+  }
+}
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerChoicePrompt = prompt("Enter Rock, Paper or Scissors");
+    playRound(playerChoicePrompt, getComputerChoice());
+    console.log(wins + loses + ties);
+  }
+
+  console.log("You won " + wins + ", lost " + loses + " and tied " + ties + " games.");
+}
+
+const computerChoice = getComputerChoice();
+
+game();
